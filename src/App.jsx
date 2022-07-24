191bd916036a84cd8ccd34e99a15e18e994b14b3
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-24 13:49:44 +0600

    feat(*): initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from 'react'
 import "./App.css";
+import Footer from "./components/Footer"
 
 function App() {
+  const [quote, setQuote] = useState({})
+
+  const getQuote = () => {
+    fetch('https://programming-quotes-api.herokuapp.com/quotes/random')
+      .then(res => res.json())
+      .then(data => setQuote(data))
+  }
+
+  useEffect(() => {
+    getQuote()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote">
+        <h1>{quote.en}</h1>
+        <p>~ {quote.author}</p>
+        <button onClick={getQuote}>Get a New One</button>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Footer.css"
+
+function Footer() {
+  return (
+    <footer>
+        <span>UI by <a href="https://dosek.xyz/">Dositan</a></span>
+        <br />
+        <span>API by <a href="https://github.com/skolakoda/programming-quotes-api">Skola Koda</a></span>
+    </footer>
+  )
+}
+
+export default Footer
